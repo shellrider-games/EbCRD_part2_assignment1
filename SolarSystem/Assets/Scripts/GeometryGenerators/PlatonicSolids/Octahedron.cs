61576502145a2ab14f5c4817bdbc1ca45466b7a0
Georg Becker <Georg.Becker@gmx.at>
2023-05-06 09:38:55 +0200

    Create Octahedron

---
+++ SolarSystem/Assets/Scripts/GeometryGenerators/PlatonicSolids/Octahedron.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Octahedron : MonoBehaviour
+{
+    [SerializeField] private Material material;
+
+    private MeshRenderer _meshRenderer;
+    private MeshFilter _meshFilter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateMeshComponents();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void GenerateMeshComponents()
+    {
+        _meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        _meshRenderer.sharedMaterial = material;
+        _meshFilter = gameObject.AddComponent<MeshFilter>();
+        _meshFilter.mesh = GenerateMesh();
+    }
+
+    private Mesh GenerateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = GenerateVertices();
+        // The multiplication and adding takes place because I need individual vertices to achieve a flat shaded look
+        mesh.triangles = new[]
+        {
+            0, 2, 1,
+            0 + 6 * 1, 3, 2 + 6 * 1,
+            0 + 6 * 2, 4, 3 + 6 * 1,
+            0 + 6 * 3, 1 + 6 * 1, 4 + 6 * 1,
+            5, 1 + 6 * 2, 2 + 6 * 2,
+            5 + 6 * 1, 2 + 6 * 3, 3 + 6 * 2,
+            5 + 6 * 2, 3 + 6 * 3, 4 + 6 * 2,
+            5 + 6 * 3, 4 + 6 * 3, 1 + 6 * 3
+        };
+        mesh.RecalculateNormals();
+        return mesh;
+    }
+
+    private Vector3[] GenerateVertices()
+    {
+        // To get 8 flat shaded surfaces we need 3*8 vertices due to how shading in unity uses the vertex normal and
+        Vector3[] vertices = new Vector3[3 * 8]
+        {
+            new Vector3(0f, .5f, 0f),
+            new Vector3(-.5f, 0f, 0f),
+            new Vector3(0f, 0f, -.5f),
+            new Vector3(.5f, 0f, 0f),
+            new Vector3(0f, 0f, .5f),
+            new Vector3(0f, -.5f, 0f),
+            new Vector3(0f, .5f, 0f),
+            new Vector3(-.5f, 0f, 0f),
+            new Vector3(0f, 0f, -.5f),
+            new Vector3(.5f, 0f, 0f),
+            new Vector3(0f, 0f, .5f),
+            new Vector3(0f, -.5f, 0f),
+            new Vector3(0f, .5f, 0f),
+            new Vector3(-.5f, 0f, 0f),
+            new Vector3(0f, 0f, -.5f),
+            new Vector3(.5f, 0f, 0f),
+            new Vector3(0f, 0f, .5f),
+            new Vector3(0f, -.5f, 0f),
+            new Vector3(0f, .5f, 0f),
+            new Vector3(-.5f, 0f, 0f),
+            new Vector3(0f, 0f, -.5f),
+            new Vector3(.5f, 0f, 0f),
+            new Vector3(0f, 0f, .5f),
+            new Vector3(0f, -.5f, 0f)
+        };
+        
+        
+        return vertices;
+    }
+}
